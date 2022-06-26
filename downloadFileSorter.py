2d0ec5a25a9b7904097bcc7a20dbcf77aef35f75
Saud0227 <saud02tech@gmail.com>
2022-06-27 00:00:13 +0200

    file logging made to verbose version

--- downloadFileSorter.py
+++ downloadFileSorter.py
@@ -10,24 +10,38 @@ from sys import exit
 from plyer import notification
 # from typing_extensions import runtime
 
-active = True
+import logging
+logging.basicConfig(filename='app.log', format='%(asctime)s - %(message)s', level=logging.INFO)
+
+
 # Checks files while true
-runtime = 0
+active = True
+
 # Number of times main loop has run, 1/sec
-tToCheck = 10
+runtime = 0
+
 # Checks for files at 0
-cc = 0
+tToCheck = 10
+
 # Number of checks the program has done
-sh = False
+cc = 0
+
 # When true, the program shutdown the next loop
-iC = False
+sh = False
+
 # When false, sort all items regardless of when they
 # were created. This flag is kept at false unless
 # executed via command from secondary application
+forceCheckFlag = False
 
 
-pa = p.Path.home()
-tDir = pa / "Downloads"
+logP = p.Path.cwd() / 'logFiles'
+if p.Path.is_dir(logP) is False:
+    os.mkdir('logFiles')
+    logging.warning(f'Made logFiles dir at {logP}')
+
+
+tDir = p.Path.home() / "Downloads"
 # C:\Users\CarlJ\Downloads
 suffix = [".zip"]
 
@@ -39,31 +53,68 @@ def checkFold():
             os.mkdir(i[1:])
             dirC.append(tDir / i[1:])
     for j in dirC:
-        print(str(j) + " was created")
+        logging.info(str(j) + " was created")
 
 os.chdir(tDir)
 
 
+def writeOutputFiles(toWrite):
+    global logP
+    timeStamp = str(datetime.datetime.now()).split('.')[0].split()
+
+    hhMMss = timeStamp[1].split(':')
+    timeStamp[1] = ''.join(hhMMss[0] + '.' + hhMMss[1])
+
+    nameSpace = ''.join(timeStamp[0] + '_' + timeStamp[1])
+
+    outStr = ''
+    outStr += f'{len(toWrite)} item were sorted:'
+
+    for i in toWrite:
+        outStr += f'\n - {i}'
+
+    getFileName = nameSpace + '.log'
+    with open(getFileName, 'w') as f:
+        f.write(outStr)
+    os.rename(str(getFileName), str(logP / getFileName))
+    return str(logP / getFileName)
+
 
 
 def mainloop():
-    nFSorted=0
+    fSorted = []
     filesToSort = [f for f in os.listdir(tDir) if p.Path( tDir / f ).is_file()]
     for fil in filesToSort:
         fP=p.Path(tDir / fil)
         fSuffix = fP.suffix
-        if(fSuffix not in suffix):
-            suffix.append(fSuffix)
-        checkFold()
         fts=fP.stat().st_ctime
         fTS=datetime.datetime.fromtimestamp(fts)
         cT=datetime.datetime.today()
         difT = cT - fTS
-        if difT.days>3 or iC:
-            os.rename(str(fP),str(tDir / fSuffix[1:] / fP.parts[-1]))
-            nFSorted+=1
-    if nFSorted>0:
-        sendNot(str(nFSorted) + " files were sorted.",50)
+        if difT.days>3 or forceCheckFlag:
+            if(fSuffix not in suffix):
+                suffix.append(fSuffix)
+                checkFold()
+            try:
+                os.rename(str(fP),str(tDir / fSuffix[1:] / fP.parts[-1]))
+                fSorted.append(fP.parts[-1])
+            except FileExistsError:
+                logging.warning(str(fP.parts[-1]) + ' already exists, testing numerals')
+                for i in range(1,10):
+                    fileN = fP.parts[-1].split('.')
+                    fileNIndex = fileN[0] + f'({i}).' +fileN[1]
+                    try:
+                        os.rename(str(fP),str(tDir / fSuffix[1:] / fileNIndex))
+                        fSorted.append(fileNIndex)
+                        break
+                    except FileExistsError:
+                        logging.warning(fileNIndex + ' already exists')
+                        if i > 9:
+                            logging.critical(str(fP.parts[-1]) + ' could not be sorted, skipped')
+
+    if len(fSorted)>0:
+        moveLogFile = writeOutputFiles(fSorted)
+        logging.info(f"{len(fSorted)} files were sorted, move specific log file at {moveLogFile}")
 
 # RPyC service definition
 
@@ -81,9 +132,9 @@ def toggle(_input):
     if isinstance(_input, bool) and active != _input:
         active = _input
         if active:
-            sendNot("Download sorter resumed work", 10)
+            logging.info("Download sorter resumed work")
         else:
-            sendNot("Download sorter paused",10)
+            logging.info("Download sorter paused using console")
         return("status: " + str(active))
     elif active == _input:
         return ("Status is already set to " + str(active))
@@ -111,18 +162,18 @@ class MyService(rpyc.Service):
         return [runtime, cc]
     def exposed_close(self):
         global sh
-        sendNot("Download sorter process aborted", 50)
+        logging.info("Download sorter process closed frome console")
         sh = True
 
     def exposed_triggerCheck(self):
-        global iC
-        iC=True
+        global forceCheckFlag
+        forceCheckFlag=True
         return("Check triggered")
 
 
 
 
-print("Starting rpyc")
+logging.info("Starting rpyc")
 # start the rpyc server
 from rpyc.utils.server import ThreadedServer
 from threading import Thread
@@ -130,28 +181,28 @@ server = ThreadedServer(MyService, port = 12345)
 th = Thread(target = server.start)
 th.daemon = True
 th.start()
-print("rpyc started")
+logging.info("rpyc started")
 
-
+"""
 def sendNot(_text, _time):
     if not isinstance(_time, (float,int)) and _time < 10:
         _time = 10
     notification.notify(title = "Download Sorter", message = _text, timeout = _time)
-
-sendNot("Download sorter initiated",10)
+"""
+logging.info("Download sorter initiated")
 while True:
     if sh:
         exit()
     runtime+=1
     tToCheck-=1
-    if (active and tToCheck<=0) or iC:
+    if (active and tToCheck<=0) or forceCheckFlag:
         mainloop()
-        if iC:
-            print("IC triggered by cmd application")
-            iC=False
+        if forceCheckFlag:
+            logging.info("Force check triggered by cmd application")
+            forceCheckFlag=False
         cc+=1
         tToCheck=100
-    print(runtime,active)
+    print(tToCheck, str(p.Path.cwd()))
     # active= not active
     t.sleep(1)
 
